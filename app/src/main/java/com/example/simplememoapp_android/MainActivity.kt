6f0a0388a74743ecabc3c2b3856e22be32c85349
kazuya-dev0707 <takahashi58586969@gmail.com>
2025-10-07 13:55:38 +0900

    refactor: Move AppNavHost to navigation package

--- app/src/main/java/com/example/simplememoapp_android/MainActivity.kt
+++ app/src/main/java/com/example/simplememoapp_android/MainActivity.kt
@@ -9,7 +9,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
-import com.example.simplememoapp_android.ui.navigation.AppNavHost
+import com.example.simplememoapp_android.navigation.AppNavHost
 import com.example.simplememoapp_android.ui.theme.SimpleMemoAppAndroidTheme
 import dagger.hilt.android.AndroidEntryPoint
 
